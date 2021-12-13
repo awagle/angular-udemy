57e553d0cdad0db4e8905077dd7dbc79cbf728c0
Aditya Wagle <awagle@tibco.com>
2021-12-13 11:37:40 +0530

    Completed Project - The Basics module which includes creating and adding components, iterating with ng-for etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipelist/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shoppinglist/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppinglistComponent,
+    RecipelistComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipelist',
+  templateUrl: './recipelist.component.html',
+  styleUrls: ['./recipelist.component.css']
+})
+export class RecipelistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe','This is a test',"https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
